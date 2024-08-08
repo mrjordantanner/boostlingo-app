0cdf95b2f50ff8265d2c1ceeea75bd0d55f8d427
Jordan Smith <mrjordantanner@gmail.com>
2024-08-08 09:38:44 -0500

    Implement basic JsonDataService

---
+++ src/JsonDataService.cs
@@ -0,0 +1,20 @@
+﻿
+namespace boostlingo
+{
+    public class JsonDataService
+    {
+        private readonly HttpClient _httpClient;
+
+        public JsonDataService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> GetJsonDataAsync(string url)
+        {
+            var response = await _httpClient.GetStringAsync(url);
+            return response;
+        }
+    }
+
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace boostlingo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddHttpClient();
+                    services.AddTransient<JsonDataService>();
+                })
+                .Build();
+
+            var dataService = host.Services.GetRequiredService<JsonDataService>();
+
+            var url = "https://microsoftedge.github.io/Demos/json-dummy-data/64KB.json";
+            var response = await dataService.GetJsonDataAsync(url);
+            Console.WriteLine(response);
+
+        }
+    }
+}
